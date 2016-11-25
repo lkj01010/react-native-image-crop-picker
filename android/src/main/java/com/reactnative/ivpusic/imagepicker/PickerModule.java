b0388c5e62ecb0c57a28cf998bad35462fa9e96d
Midstream <lkj01010@163.com>
2016-11-26 01:17:13 +0800

    ［android］增加图片压缩功能（不cropping的图进行压缩）

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -33,6 +33,7 @@ import com.reactnativenavigation.controllers.NavigationActivity;
 import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.*;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
@@ -373,7 +374,8 @@ public class PickerModule extends ReactContextBaseJavaModule implements Activity
             return getVideo(path, mime);
         }
 
-        return getImage(activity, uri, true);
+//        return getImage(activity, uri, true);
+        return getCompressedImage(activity, uri, true);
     }
 
     public WritableMap getVideo(String path, String mime) {
@@ -428,6 +430,110 @@ public class PickerModule extends ReactContextBaseJavaModule implements Activity
 
         if (includeBase64) {
             image.putString("data", getBase64StringFromFile(path));
+        }
+
+        return image;
+    }
+
+    public static Bitmap getBitmapFormUri(Activity ac, Uri uri) throws FileNotFoundException, IOException {
+        InputStream input = ac.getContentResolver().openInputStream(uri);
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;
+        onlyBoundsOptions.inDither = true;//optional
+        onlyBoundsOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
+        int originalWidth = onlyBoundsOptions.outWidth;
+        int originalHeight = onlyBoundsOptions.outHeight;
+        if ((originalWidth == -1) || (originalHeight == -1))
+            return null;
+        //图片分辨率以480x800为标准
+        float hh = 800f;//这里设置高度为800f
+        float ww = 800f;//这里设置宽度为480f
+        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (originalWidth > originalHeight && originalWidth > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (originalWidth / ww);
+        } else if (originalWidth < originalHeight && originalHeight > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (originalHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        //比例压缩
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = be;//设置缩放比例
+        bitmapOptions.inDither = true;//optional
+        bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
+        input = ac.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+
+//        return compressImage(bitmap);//再进行质量压缩
+        return bitmap;
+    }
+
+    public static ByteArrayOutputStream compressImage(Bitmap image) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 100;
+        while (baos.toByteArray().length / 1024 > 120) {  //循环判断如果压缩后图片是否大于100kb,大于继续压缩
+            baos.reset();//重置baos即清空baos
+            //第一个参数 ：图片格式 ，第二个参数： 图片质量，100为最高，0为最差  ，第三个参数：保存压缩后的数据的流
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+            options -= 10;//每次都减少10
+        }
+        return baos;
+//        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
+//        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+//        return bitmap;
+    }
+
+    private WritableMap getCompressedImage(Activity activity, Uri uri, boolean resolvePath) throws Exception {
+        WritableMap image = new WritableNativeMap();
+        String path = uri.getPath();
+
+        if (resolvePath) {
+            path = RealPathUtil.getRealPathFromURI(activity, uri);
+        }
+
+        if (path == null || path.isEmpty()) {
+            throw new Exception("Cannot resolve image path.");
+        }
+
+        if (path.startsWith("http://") || path.startsWith("https://")) {
+            throw new Exception("Cannot select remote files");
+        }
+//
+//        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inJustDecodeBounds = true;
+////
+//        BitmapFactory.decodeFile(path, options);
+//        if (options.outMimeType == null || options.outWidth == 0 || options.outHeight == 0) {
+//            throw new Exception("Invalid image selected");
+//        }
+
+
+        Bitmap imageData = getBitmapFormUri(activity, uri);
+        ByteArrayOutputStream os = compressImage(imageData);
+
+        String imageFilename = "image-" + UUID.randomUUID().toString() + ".jpg";
+        File tmpImagePath = new File(this.getTmpDir(), imageFilename);
+
+        // Generate compressed image file
+        FileOutputStream fos = new FileOutputStream(tmpImagePath);
+        fos.write(os.toByteArray());
+        fos.flush();
+        fos.close();
+
+        image.putString("path", "file://" + tmpImagePath);
+        image.putInt("width", imageData.getWidth());
+        image.putInt("height", imageData.getHeight());
+        image.putString("mime", getMimeType(path));
+        image.putInt("size", (int) tmpImagePath.length());
+
+        if (includeBase64) {
+            image.putString("data", Base64.encodeToString(os.toByteArray(), Base64.NO_WRAP));
         }
 
         return image;
@@ -512,7 +618,8 @@ public class PickerModule extends ReactContextBaseJavaModule implements Activity
                 startCropping(activity, uri);
             } else {
                 try {
-                    mPickerPromise.resolve(getImage(activity, uri, true));
+//                    mPickerPromise.resolve(getImage(activity, uri, true));
+                    mPickerPromise.resolve(getCompressedImage(activity, uri, true));
                 } catch (Exception ex) {
                     mPickerPromise.reject(E_NO_IMAGE_DATA_FOUND, ex.getMessage());
                 }
